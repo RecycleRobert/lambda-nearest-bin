ad0a1c68dc62831f2ec3f2e65be76e99e9dc4a87
RootieWL <kwekwl1991@gmail.com>
2021-03-25 03:01:09 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const Telegraf = require('telegraf');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '1790342466:AAEn44buxsnMHzbyacTcbbxx0rq7myeEOUM';
+const token = //insert token here
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot_tb = new TelegramBot(token, {polling: true});
