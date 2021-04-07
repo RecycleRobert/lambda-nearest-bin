5ff7ac37d6a1c2803d52da0483af583b4df562b7
aaronteoh <teohkangkaiaaron@gmail.com>
2021-04-08 00:04:52 +0800

    updated env variables

--- function/index.js
+++ function/index.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 exports.handler = async function(event, context) {
   const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
   });
   
   connection.connect();
